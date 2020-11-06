f5c36c5d24e35f0905b4d07a066853aedbffd288
gdqyhezhiyong <510436948@qq.com>
2020-11-06 17:11:14 +0800

    delete project baozhang

--- baozhang/src/gdqy/edu/cn/service/UserServices.java
+++
@@ -1,7 +0,0 @@
-package gdqy.edu.cn.service;
-
-public interface UserServices {
-	public String login(String username,String password);
-	public boolean chang_password(String username,String password);
-	
-}
